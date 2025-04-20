e0890feb68ab7a05d7b5bc0c223e3a48fc7b5748
fc58177 <fc58177@alunos.fc.ul.pt>
2025-04-20 22:38:19 +0100

    发现新结构，代码重构！”

--- PSI_projeto/frontend/taxi-manager/src/app/manager/report/report.module.ts
+++ PSI_projeto/frontend/taxi-manager/src/app/manager/report/report.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 
 import { ReportRoutingModule } from './report-routing.module';
-import { ReportDashboardComponent } from './report-dashboard/report-dashboard.component';
-import { DriverDetailComponent } from './driver-detail/driver-detail.component';
-import { TaxiDetailComponent } from './taxi-detail/taxi-detail.component';
-import { TripDetailComponent } from './trip-detail/trip-detail.component';
+import { ReportDashboardComponent } from './driver-part/report-dashboard/report-dashboard.component';
+import { DriverDetailComponent } from './driver-part/driver-detail/driver-detail.component';
+import { TaxiDetailComponent } from './driver-part/taxi-detail/taxi-detail.component';
+import { TripDetailComponent } from './driver-part/trip-detail/trip-detail.component';
 
 
 @NgModule({
