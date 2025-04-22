ce1a2f08b2c52ed2bd1d97ed8d603e65ab6ed39b
fc58177 <fc58177@alunos.fc.ul.pt>
2025-04-22 17:50:40 +0100

    “稍微有感觉了，加油，存个档”

--- PSI_projeto/frontend/taxi-manager/src/app/manager/taxi/taxi.module.ts
+++ PSI_projeto/frontend/taxi-manager/src/app/manager/taxi/taxi.module.ts
@@ -7,6 +7,13 @@ import { TaxiListComponent } from './taxi-list/taxi-list.component';
 import { TaxiFormComponent } from './taxi-form/taxi-form.component';
 import { EditTaxiComponent } from './edit-taxi/edit-taxi.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'; // 可选：支持错误图标显示
+import { MatOptionModule } from '@angular/material/core'; // 重要：确保 mat-option 识别
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +24,15 @@ import { EditTaxiComponent } from './edit-taxi/edit-taxi.component';
   imports: [
     CommonModule,
     TaxiRoutingModule,
-    FormsModule
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+    MatOptionModule,
+    MatButtonModule
   ]
 })
 export class TaxiModule { }
