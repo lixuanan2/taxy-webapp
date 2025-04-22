3ce507197c17c8c6325c0240acd2d594190d4eb7
fc58177 <fc58177@alunos.fc.ul.pt>
2025-04-22 19:10:47 +0100

    不错啊，效率提升上来了，存个档”

--- PSI_projeto/frontend/taxi-manager/src/app/manager/price/price.module.ts
+++ PSI_projeto/frontend/taxi-manager/src/app/manager/price/price.module.ts
@@ -6,6 +6,12 @@ import { PriceRoutingModule } from './price-routing.module';
 import { PriceFormComponent } from './price-form/price-form.component';
 import { PriceListComponent } from './price-list/price-list.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'; // 可选：支持错误图标显示
+import { MatOptionModule } from '@angular/material/core'; // 重要：确保 mat-option 识别
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     PriceFormComponent,
@@ -14,7 +20,15 @@ import { PriceListComponent } from './price-list/price-list.component';
   imports: [
     CommonModule,
     FormsModule,
-    PriceRoutingModule
+    PriceRoutingModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+    MatOptionModule,
+    MatButtonModule
   ]
 })
 export class PriceModule { }
